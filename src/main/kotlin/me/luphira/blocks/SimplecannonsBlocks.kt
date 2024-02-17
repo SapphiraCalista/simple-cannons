879463acbfaa2dccad6fa1f7d20dcb5d42c730b7
illucc <illucizer@gmail.com>
2024-02-17 21:45:18 +0200

    :bug: fixed charged dispenser direction

--- src/main/kotlin/me/luphira/blocks/SimplecannonsBlocks.kt
+++ src/main/kotlin/me/luphira/blocks/SimplecannonsBlocks.kt
@@ -1,20 +1,26 @@
 package me.luphira.blocks
 
 import me.luphira.Simplecannons
+import me.luphira.blocks.custom.ChargedDispenser
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
-import net.minecraft.block.Block
-import net.minecraft.block.Material
+import net.minecraft.block.*
 import net.minecraft.item.BlockItem
 import net.minecraft.item.ItemGroup
+import net.minecraft.item.ItemPlacementContext
+import net.minecraft.state.StateManager
 import net.minecraft.util.Identifier
+import net.minecraft.util.math.BlockPos
 import net.minecraft.util.registry.Registry
+import net.minecraft.util.shape.VoxelShape
+import net.minecraft.util.shape.VoxelShapes
+
 
 class SimplecannonsBlocks {
     // Function to make registering look a lot nicer
-    private fun register(name: String, block: Block, itemGroup: ItemGroup) {
+    private fun register(name: String, block: Block, itemGroup: ItemGroup): Block? {
         registerItem(name, block, itemGroup)
-        Registry.register(Registry.BLOCK, Identifier(Simplecannons.modid, name), block)
+        return Registry.register(Registry.BLOCK, Identifier(Simplecannons.modid, name), block)
     }
     private fun registerItem(name: String, block: Block, itemGroup: ItemGroup) {
         Registry.register(Registry.ITEM, Identifier(Simplecannons.modid, name),
@@ -24,7 +30,10 @@ class SimplecannonsBlocks {
     fun registerBlocks() {
         Simplecannons.logger.info("Simple Cannons will now begin initialising blocks.")
         // To do: Create a function to register blocks to prevent horrors later.
-        register("charged_dispenser", Block(FabricBlockSettings.of(Material.STONE).strength(6f)), ItemGroup.REDSTONE)
+        //register("charged_dispenser", ChargedDispenser(FabricBlockSettings.of(Material.STONE).strength(6f)), ItemGroup.REDSTONE)
         //register("charged_dispenser", ChargedDispenser, ItemGroup.REDSTONE) // The replacement dispenser, does not work.
+
+        // Its a value so if we need it later its there :sunglasses: also hai :3
+        val CHARGED_DISPENSER = register("charged_dispenser", ChargedDispenser(FabricBlockSettings.of(Material.STONE).strength(6f)), ItemGroup.REDSTONE)
     }
 }
--- src/main/kotlin/me/luphira/blocks/custom/ChargedDispenser.kt
+++ src/main/kotlin/me/luphira/blocks/custom/ChargedDispenser.kt
@@ -3,38 +3,35 @@ package me.luphira.blocks.custom
 import me.luphira.Simplecannons
 import net.minecraft.block.Block
 import net.minecraft.block.BlockState
+import net.minecraft.block.FacingBlock
 import net.minecraft.item.ItemPlacementContext
 import net.minecraft.state.StateManager
 import net.minecraft.state.property.DirectionProperty
 import net.minecraft.state.property.Properties
 import net.minecraft.util.BlockMirror
 import net.minecraft.util.BlockRotation
+import net.minecraft.util.math.Direction
 
-class ChargedDispenser(settings: Settings?) : Block(settings) {
-    val FACING: DirectionProperty = Properties.HORIZONTAL_FACING
+class ChargedDispenser(settings: Settings?) : FacingBlock(settings) {
+    val FACING: DirectionProperty = Properties.FACING
+
+    //TODO: top texture for the charged dispenser
+
+    init {
+        defaultState = defaultState.with(Properties.FACING, Direction.NORTH)
+    }
+
     override fun getPlacementState(ctx: ItemPlacementContext?): BlockState? {
+        //TODO: Dispensers cannot be placed facing up
         return this.defaultState.with(FACING, ctx?.playerFacing?.opposite)
-    }
-
-    override fun rotate(state: BlockState?, rotation: BlockRotation?): BlockState? {
-        if (state != null) {
-            return state.with(FACING, rotation?.rotate(state.get(FACING)))
-        } else {
-            return state
-        }
-    }
-
-    override fun mirror(state: BlockState?, mirror: BlockMirror?): BlockState? {
-        if (state != null && mirror != null) {
-            return state.rotate(mirror.getRotation(state.get(FACING)))
-        } else {
-            Simplecannons.logger.info("Something has gone seriously wrong with Simple Cannons.")
-            return state
-        }
     }
 
     override fun appendProperties(builder: StateManager.Builder<Block, BlockState>?) {
         super.appendProperties(builder)
-        builder?.add(FACING)
+        builder?.add(Properties.FACING)
+    }
+
+    companion object {
+
     }
 }
